24f0462b8b64a0b22def6dba401bedf22eb04be5
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-09-19 14:27:18 +0530

    Update Imagegenerator.jsx

--- src/components/Pages/Imagegenerator.jsx
+++ src/components/Pages/Imagegenerator.jsx
@@ -9,7 +9,7 @@ const ImageGenerator = () => {
   const generateImage = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`YOUR_API_ENDPOINT?text=${inputText}`);
+      const response = await fetch(`http://20.185.69.17:8000/gan/generate-image/?text=${inputText}`);
       const data = await response.json();
       const generatedImageUrl = data.imageUrl;
       setImageUrl(generatedImageUrl);
