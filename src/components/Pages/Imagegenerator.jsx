7217991322b9d899787209e491ddbdf5332454f3
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-09-21 18:42:32 +0530

    Update Imagegenerator.jsx

--- src/components/Pages/Imagegenerator.jsx
+++ src/components/Pages/Imagegenerator.jsx
@@ -9,7 +9,7 @@ const ImageGenerator = () => {
   const generateImage = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://20.185.69.17:8000/?text=${inputText}", {
+      const response = await fetch("http://20.185.69.17:8000/gan/generate-image/", {
     // Adding method type
     method: "POST",
     // Adding body or contents to send
