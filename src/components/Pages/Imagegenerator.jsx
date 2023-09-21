25a4da13a1253cd94d4a63fccea171a1f1b4f183
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-09-21 18:59:56 +0530

    Update Imagegenerator.jsx

--- src/components/Pages/Imagegenerator.jsx
+++ src/components/Pages/Imagegenerator.jsx
@@ -13,7 +13,7 @@ const ImageGenerator = () => {
     // Adding method type
     method: "POST",
     // Adding body or contents to send
-    body: JSON.stringify(inputText),
+    body: JSON.stringify({"text": inputText}),
     // Adding headers to the request
     headers: {"Content-type": "application/json; charset=UTF-8"}
 })
