ed1b5920edd6e9073a1daa57649187f2527eb8fc
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-10-06 13:48:37 +0530

    Update Imagegenerator.jsx

--- src/components/Pages/Imagegenerator.jsx
+++ src/components/Pages/Imagegenerator.jsx
@@ -13,7 +13,7 @@ const ImageGenerator = () => {
       const response = await fetch(`http://20.121.114.90:8000/gan/generate-image/`, {method: "POST", body: JSON.stringify({"text": inputText})});
       const data = await response.text();
       
-      const dataUri = `data:image/jpeg;base64,${generatedImageUrl}`;
+      const dataUri = `data:image/jpeg;base64,${data}`;
       
       setImageUrl(dataUri);
     } catch (error) {
