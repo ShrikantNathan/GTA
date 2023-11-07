f1c6337ce02614678dac37325acb3fd9c71b46d7
Shrikant Nathan <66513107+ShrikantNathan@users.noreply.github.com>
2023-11-07 18:17:34 +0530

    Add files via upload

--- src/components/Pages/CloudComputing.jsx
+++ src/components/Pages/CloudComputing.jsx
@@ -5,7 +5,6 @@ import database from '../../assest/Database-img.png'
 import vectorimg from '../../assest/Vectorimg.png'
 import GoToTop from './Top'
 
-
 function CloudComputing() {
     return (
         <div>
@@ -18,13 +17,16 @@ function CloudComputing() {
                     <div className='imagecloud'>
                         <img src={database} alt='databseimg' height={300}></img>
                     </div>
-                    <p className='cloudp p-10'>Plan, participate in or execute assigned audits and special projects       including investigations Perform risk analysis of functions and activities to determine the nature of operations and the adequacy of the system of internal control to achieve established objectives
+
+                    <p className='cloudp p-10'>
+                        <h3>Cutting-Edge Cloud Computing  – Machine Learning and Artificial Intelligence </h3>
+                        Your ultimate ally in transforming your operational dynamics with cutting-edge cloud computing tailored explicitly for Machine Learning (ML) and Artificial Intelligence (AI). Specialized in serving a broad spectrum ranging from the commercial market to the federal government, we are the architects of excellence in cultivating technologically advanced solutions that resonate with innovation and strategic vision.{/* Plan, participate in or execute assigned audits and special projects       including investigations Perform risk analysis of functions and activities to determine the nature of operations and the adequacy of the system of internal control to achieve established objectives
                         Develop, perform, and document testing and walkthroughs of IT processes and controls
                         Evaluate the adequacy of the security and processing controls as they relate to each audit, and the effectiveness of general and application computer controls in effect in the IT environment
-                        Provide auditing and information systems technical assistance</p>
+                        Provide auditing and information systems technical assistance*/}</p>
                     <div className="responsive-card">
                         <p className="card-content">
-                            <ul >
+                            {/* <ul >
                                 <li>
                                     Manage and prioritize multiple assignments including all audits and projects
                                 </li>
@@ -60,20 +62,26 @@ function CloudComputing() {
                                 <br />
                                 <li>
                                     Analyze data utilization capabilities, allowing users to access, manipulate, create, and share data seamlessly
-                                </li>
-                                <div className="vectorimg">
-                                    <img alt="vectorimg" src={vectorimg} style={{ opacity: 0.50 }} width={118}></img>
-                                </div>
-
-                            </ul>
-                        </p>
-                    </div>
+                                </li> */}
+                            <h3>Innovation Through Expertise</h3>
+                            <p>Our operation's heart lies in a dedicated team of seasoned over 200 data scientists and software engineering experts. With a penchant for innovation and a wealth of curated knowledge, our professionals are instrumental in leveraging state-of-the-art ML and AI technologies to deliver bespoke solutions that drive efficiency, innovation, and strategic insights.
+                            </p><h3>Customized Cloud Computing</h3>
+                            <p>Harness the power of the cloud with our exceptional cloud computing services. Focused on creating robust, scalable, and secure cloud infrastructures, we ensure your ML and AI applications run seamlessly, unlocking unparalleled performance and reliability. Tailored to meet the unique demands of your projects, our configurations are the bedrock upon which sustainable and transformative technologies flourish.
+                            </p><h3>Commitment to Excellence in the Commercial and Federal Domains</h3>
+                            <p>Our expertise transcends boundaries, catering to the commercial market and federal government sectors. Committed to delivering excellence, our services are meticulously designed to meet different organizations' nuanced and critical needs, ensuring they thrive in an era of technological advancements and data-driven decision-making.
+                            </p><h3>Strategic Partnerships for Success
+                            </h3><p>Collaborate with us to forge a strategic alignment and mutual growth partnership. Our holistic approach ensures that your organization is always at the forefront of technological advancements in ML and AI, fostering an ecosystem that embodies success, sustainability, and continuous improvement.
+                            </p>
+                            <div className="vectorimg">
+                                <img alt="vectorimg" src={vectorimg} style={{ opacity: 0.50 }} width={118}></img>
+                            </div>
+                        {/* </ul> */}
+                    </p>
                 </div>
-                <GoToTop />
             </div>
+            <GoToTop />
         </div>
+        </div >
     )
 }
-
-
 export default CloudComputing;
